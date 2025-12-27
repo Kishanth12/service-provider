0ec0193fbc6e21e3bab13f8b0f97a697860d9efa
kishanth12 <kishanth1209@gmail.com>
2025-12-27 12:56:05 +0530

    chore(nest): setup main bootstrap with global pipes, filters, interceptors, and logger

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MyLoggerService } from './my-logger/my-logger.service';
+import { AllExceptionsFilter } from './all-exceptions';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove unknown fields
+      forbidNonWhitelisted: true, // throw error if extra fields sent
+      transform: true, // auto type conversion
+    }),
+  );
+
+  app.useLogger(app.get(MyLoggerService));
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
