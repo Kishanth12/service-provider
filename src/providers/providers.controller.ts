2735a5d4677743b4d9bb3566e0d74c5ab317d825
kishanth12 <kishanth1209@gmail.com>
2025-12-28 21:58:18 +0530

    feat(providers): create providers controller and routes

---
+++ src/providers/providers.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProvidersService } from './providers.service';
+import { UpdateProviderDto } from './dto/update-provider-dto';
+
+@Controller('providers')
+export class ProvidersController {
+  constructor(private readonly providersService: ProvidersService) {}
+
+  @Get()
+  async findAll() {
+    const providers = await this.providersService.findAll();
+    return { message: 'Providers retrieved successfully', data: providers };
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const provider = await this.providersService.findOne(id);
+    return { message: 'Provider retrieved successfully', data: provider };
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateProviderDto: UpdateProviderDto,
+  ) {
+    const provider = await this.providersService.update(id, updateProviderDto);
+    return { message: 'Provider updated successfully', data: provider };
+  }
+
+  @Patch(':id/approve')
+  async approve(@Param('id') id: string) {
+    const provider = await this.providersService.approveProvider(id);
+    return { message: 'Provider approved successfully', data: provider };
+  }
+
+  @Patch(':id/reject')
+  async reject(@Param('id') id: string) {
+    const result = await this.providersService.rejectProvider(id);
+    return { message: result.message };
+  }
+
+  @Post('apply/:userId')
+  async apply(@Param('userId') userId: string) {
+    const provider = await this.providersService.applyForProvider(userId);
+    return {
+      message: 'Provider application submitted successfully',
+      data: provider,
+    };
+  }
+  @Get('pending/applications')
+  async findPending() {
+    const providers = await this.providersService.findPending();
+    return {
+      message: 'Pending provider applications retrieved successfully',
+      data: providers,
+    };
+  }
+}
