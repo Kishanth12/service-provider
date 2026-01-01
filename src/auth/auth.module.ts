8e18b95ec58cdd67e0fa6b1cc3a74f949f4e87d6
kishanth12 <kishanth1209@gmail.com>
2026-01-01 19:08:20 +0530

    feat(auth): configure JWT module and export auth guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [
     DatabaseModule,
     JwtModule.register({
+      global: true, // Making it global makes it easier to use in guards
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtAuthGuard, RolesGuard],
   controllers: [AuthController],
+  exports: [JwtAuthGuard, RolesGuard], // Export them so other modules can use them
 })
 export class AuthModule {}
